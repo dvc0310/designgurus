4c51a6769b62d704f47d11b534527a45e38b4120
dvc0310 <96139120+dvc0310@users.noreply.github.com>
2024-10-28 20:37:53 -0400

    10/28/24

---
+++ merge_intervals/intervals.py
@@ -0,0 +1,13 @@
+class Interval:
+  def __init__(self, start, end):
+    self.start = start
+    self.end = end
+
+  def __repr__(self):
+    return f"[{self.start}, {self.end}]"
+
+def convert_to_intervals(intervals):
+    interval_objects = []
+    for i in intervals:
+        interval_objects.append(Interval(i[0], i[1]))
+    return interval_objects
